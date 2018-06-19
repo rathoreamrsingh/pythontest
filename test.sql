bb019e1ec17d9e01b2517caec71b7b10a814f816
Amar Singh <amarsingh@outlook.in>
2018-06-19 23:55:29 +0530

    test.sql
    test

---
+++ test.sql
@@ -0,0 +1,23 @@
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','IBM, Mediaocean Partner to Tackle Ad Industry Opacity with Blockchain - Coindesk https://t.co/mHcfOrZPnb https://t.co/R1sTYOs1jz',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','IBM Power System L922 Introduction and Technical Overview https://t.co/S4SvzzzCe1',0.0,0.1);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @IBM: Could a machine debate a human being? 6 years ago we embarked on a mission to find out. Today, we\'re sharing the first glimpse of…',0.125,0.21666666666666667);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @NorbertElekes: CEO-to-worker pay ratio
+
+AT&amp;T: 366x
+IBM: 341x
+Intel: 211x
+Netflix: 133x
+Amazon: 59x
+Facebook: 37x
+@ElonMusk, Tesla: 0.9x…',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','Digital Education Platform Challenges Retail Learning Ecosystems Powered by IBM Watson https://t.co/fldRAuvWjU',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @innebandymag: #ibsilly: Fagerhult Habo är lyriska - före detta landslagsspelare har sajnat upp https://t.co/KrZqxcV2Vg #innebandy #floo…',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @developerWorks: Reduce your memory footprint and improve startup performance with the shared class feature. https://t.co/lRqz1lMYU0',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','#traderklub.com IBM iX and Mediaocean Bet on Blockchain to Tackle Ad Spend Opacity https://t.co/be6EdXYrRW via @CBROnline.com',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @gira79: Finally draft of @IBMRedbooks of #Power9 Scale-out server available. It was the most difficult project. Sheldon Cooper of #IBMi…',0.1,0.725);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @IBMAnalytics: "The IBM Cloud and @VMware partnership puts in place a great hybrid #cloud story, which we believe is the way of the futu…',0.8,0.75);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','Turbinado pela plataforma Watson da IBM, Paul é uma inteligência artificial que cumpre o papel de um professor 24 h… https://t.co/5em7zjdJtS',-0.6,1.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','@ScottAdamsSays off topic but just fyi #persuasion https://t.co/UmILgxaNHo https://t.co/T8RWQtFkDl',0.0,0.0);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @IBMStorage: What does a data professional need to know about #AI and its data requirements in order to support his or her organization’…',0.1,0.1);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','RT @CP24: IBM\'s AI proves formidable against 2 human debaters https://t.co/LN95bwlIZi https://t.co/TUhyySdaAV',0.15,0.05);
+INSERT INTO twitter_sentimental_analysis (company_code, tweet, polarity, subjectivity) values ('IBM','How we got to space #ptc_edu Pittsburgh Technical College students at #ibm manufacturing plant in Poughkeepsie at M… https://t.co/PgXZ8R2p9w',0.0,0.1);
